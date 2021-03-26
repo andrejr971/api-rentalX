391ce62d099b4b8b5114b167e02c5ae3a943c945
André Junior <andre.souza64@fatec.sp.gov.br>
2021-03-26 14:53:20 -0300

    Module account created

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+export default class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column({ default: false })
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
